c4c9cfd3436906d81a502e814c4843c947a7d6a8
Mikolaj <Pawlowskiem@gmail.com>
2021-06-26 20:47:44 +0200

    media theme added

--- src/Section/styled.js
+++ src/Section/styled.js
@@ -14,7 +14,7 @@ export const Header = styled.header`
     border-bottom: 1px solid rgb(235, 235, 235);
     padding: 20px;
 
-    @media(max-width:767px) {
+    @media (max-width: ${({ theme }) => theme.breakpoints.small}px) {
         grid-template-columns: 1fr;
     }
 `;
