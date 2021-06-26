4ce04017f6df12c8d8307bb350f086e3d203ee1e
Mikolaj <Pawlowskiem@gmail.com>
2021-06-27 00:24:16 +0200

    setting up flex in Form and Section

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -5,23 +5,29 @@ export const FormWrapper = styled.form`
 `;
 
 export const Fieldset = styled.fieldset`
+    margin: 0 auto;
     border: none;
-    display: grid;
-    grid-template-columns: 1fr auto;
-    padding: 15px;
-    grid-gap: 10px;
+    display: flex;
+    padding: 20px;
     background-color: white;
 
     @media (max-width: ${({ theme }) => theme.breakpoints.small}px) {
-        grid-template-columns: 1fr;
+        padding: 5px;
+        flex-direction: column;
     }
 `;
 
 export const Input = styled.input`
+    flex-grow:1;
+    margin-right: 10px;
     padding: 10px;
     color: black;
     border: 1px solid ${({ theme }) => theme.colors.primaryColor};
     outline-color: ${({ theme }) => theme.colors.buttonsColor};
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.small}px) {
+        margin: 0 0 10px;
+    }
 `;
 
 export const FormButton = styled.button`
@@ -30,16 +36,20 @@ export const FormButton = styled.button`
     padding: 10px;
     border: none;
     cursor: pointer;
-
+    flex-shrink: 0;
+    transition: background-color 0.5s, transform 0.5s;
+    
     &:hover {
-        background: ${({ theme }) => theme.colors.buttonsColorHover};
-        transform: scale (1.1);
-        transition: .5s ease-out;
+        background-color: ${({ theme }) => theme.colors.buttonsColorHover};
+        transform: scale(1.05);
     }
 
     &:active {
-        background: ${({ theme }) => theme.colors.buttonsActive};
-        transform: scale (1.05);
-        transition: .5s ease-out;
+        background-color: ${({ theme }) => theme.colors.buttonsActive};
+    }
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.small}px) {
+        flex-basis: 100%;
+        margin: 5px 0px;
     }
 `;
--- src/Section/styled.js
+++ src/Section/styled.js
@@ -7,23 +7,19 @@ export const Wrapper = styled.section`
 `;
 
 export const Header = styled.header`
-    display: grid;
-    grid-template-columns: auto auto;
-    grid-gap: 20px;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
     justify-content: space-between;
     border-bottom: 1px solid rgb(235, 235, 235);
     padding: 20px;
-
-    @media (max-width: ${({ theme }) => theme.breakpoints.small}px) {
-        grid-template-columns: 1fr;
-    }
 `;
 
 export const SectionTitle = styled.h2`
     margin: 0;
     font-size: 20px;
 
-    @media(max-width:767px) {
+    @media(max-width: ${({ theme }) => theme.breakpoints.small}px) {
     text-align: center;
     margin-left: 0;
     }
