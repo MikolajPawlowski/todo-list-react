647934f3c51e276257c7bd33d1daccb977b6e5dd
Mikolaj <Pawlowskiem@gmail.com>
2021-08-02 18:55:37 +0200

    fetchExampleTasksButton added

--- src/features/tasks/TasksPage/index.js
+++ src/features/tasks/TasksPage/index.js
@@ -5,6 +5,7 @@ import Section from "../../../common/Section";
 import Header from "../../../common/Header";
 import Container from "../../../common/Container";
 import Search from "./Search";
+import FetchExampleTasksButton from "./FetchExampleTasksButton";
 
 
 function TasksPage() {
@@ -14,6 +15,7 @@ function TasksPage() {
       <Section
         title="Dodaj nowe zadanie"
         body={<Form />}
+        extraHeaderContent={<FetchExampleTasksButton />}
       />
       <Section
         title="Wyszukiwarka"
