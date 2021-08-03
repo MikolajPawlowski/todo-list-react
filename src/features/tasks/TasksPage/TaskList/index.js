3a3f8902b7361d6b80746c8c475c2c64aff926c9
Mikolaj <Pawlowskiem@gmail.com>
2021-08-03 15:37:25 +0200

    formatting

--- src/features/tasks/TasksPage/TaskList/index.js
+++ src/features/tasks/TasksPage/TaskList/index.js
@@ -9,12 +9,11 @@ const TaskList = () => {
     const query = useQueryParameter(searchQueryParamName);
     const tasks = useSelector(state => selectTasksByQuery(state, query));
     const hideDone = useSelector(selectHideDone);
-
     const dispatch = useDispatch();
 
     return (
         <List>
-            {tasks.map(task => (
+            {tasks.map((task) => (
                 <Item
                     key={task.id}
                     hidden={task.done && hideDone}
