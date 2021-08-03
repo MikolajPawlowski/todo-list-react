a7b029d3ba636da2e8e7f61bb3f0342eaa24a257
Mikolaj <Pawlowskiem@gmail.com>
2021-08-03 15:24:00 +0200

    paragraph content edited

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -10,16 +10,16 @@ export default () => (
       title="Mikołaj Pawłowski"
       body={
         <>
-        <Paragraph>
-          <strong>Autor ćwiczy tworzenie podstron za pomocą React-Route.</strong> 
-        </Paragraph>
-        <Paragraph>
-          Jest to projekt powstały w trakcie uczestnictwa w kursie <em>Youcode - JavaScript od podstaw</em>.
-          <br>
-          Rok 2020 pokazał, że praca w wielu gałęziach gospodarki może z dnia na dzień zostać zamrożona bez naszej winy,
-          rozwijanie więc swoich umiejętności w językach programowania nikomu nie może wyjść na złe. 😉
-          </br>
-        </Paragraph>
+          <Paragraph>
+            <strong>Autor ćwiczy tu tworzenie podstron za pomocą React-Route.</strong>
+          </Paragraph>
+          <Paragraph>
+            Jest to projekt powstały w trakcie uczestnictwa w kursie <em>Youcode - JavaScript od podstaw</em>.
+          </Paragraph>
+          <Paragraph>
+            Rok 2020 pokazał, że praca w wielu gałęziach gospodarki może z dnia na dzień zostać zamrożona bez naszej winy,
+            rozwijanie więc swoich umiejętności w językach programowania nikomu nie może wyjść na złe. 😉
+          </Paragraph>
         </>
       }
     />
